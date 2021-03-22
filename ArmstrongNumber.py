356e8b765f595194ef81340947872cb16e8177c3
UtkarshSinghRajput <80973914+UtkarshSinghRajput@users.noreply.github.com>
2021-03-22 15:16:31 +0530

    Add files via upload

---
+++ ArmstrongNumber.py
@@ -0,0 +1,65 @@
+#Factorial of
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n=int(input())
+t=n
+s=0
+while n!=0:
+    a=n%10
+    s=s+a**3
+    n=n/10
+if t==s:
+    print("Armstrong Number")
+else:
+    print("Not Armstrong")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
